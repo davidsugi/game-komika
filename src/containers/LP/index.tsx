e205353c8aa8220a550ffbdb00387637f0d69311
david sugiarto <davidsugi@davids-MacBook-Air.local>
2022-01-07 14:20:17 +0700

    feat: initial comit

---
+++ src/containers/LP/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface ILPProps {
+    
+}
+
+const LP = (props:ILPProps) => {
+    return (
+        <div>
+            aaa
+        </div>
+    )
+}
+
+export default LP;
+
